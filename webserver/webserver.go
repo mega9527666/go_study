8fe96c0847c1be8216c59db5babfa372be754a93
mega@mac <mega@mac.com>
2025-03-25 21:04:40 +0800

    fix

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -1,7 +1,17 @@
 package main
 
-import "mega/engine/logger"
+import (
+	"mega/engine/logger"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Log("我是indexHandler=", w)
+}
 
 func main() {
 	logger.Log("webserver.main")
+
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":9090", nil)
 }
