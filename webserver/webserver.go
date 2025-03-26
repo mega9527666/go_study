e9e103502b1fa95480d44c39efc0ea85f75764eb
mega@mac <mega@mac.com>
2025-03-26 12:56:25 +0800

    a

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -3,15 +3,21 @@ package main
 import (
 	"mega/engine/logger"
 	"net/http"
+	// "github.com/gorilla/websocket"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	logger.Log("我是indexHandler=", w)
+	logger.Log("我是indexHandler=Host=", r.RequestURI, r.Host, r.RemoteAddr)
+}
+
+func megaHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Log("megaHandler=Host=", r.RequestURI, r.Host, r.RemoteAddr)
 }
 
 func main() {
 	logger.Log("webserver.main")
 
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/mega", megaHandler)
 	http.ListenAndServe(":9090", nil)
 }
