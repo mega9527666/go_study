d14f1174644ea51e5a3e3a903f6af1d33e1ee490
mega@mac <mega@mac.com>
2025-03-27 19:59:34 +0800

    fix

--- webserver/webreqhandler/webreqhandler.go
+++ webserver/webreqhandler/webreqhandler.go
@@ -9,31 +9,6 @@ import (
 
 type httpHandleFunc func(w http.ResponseWriter, r *http.Request)
 
-// 通用的分发函数（中间件）
-func dispatcher(next httpHandleFunc) httpHandleFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		go indexHandler(w, r, next)
-	}
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request, next httpHandleFunc) {
-	// 在执行实际的请求处理之前做一些处理
-	logger.Log("通用分发函数：请求到来，执行前处理...", r.RequestURI, r.Host, r.RemoteAddr)
-	// 你可以在这里加入公共的处理逻辑，例如验证、日志记录等
-	// 调用下一个处理函数
-	next(w, r)
-	// 在实际的请求处理之后做一些处理
-	logger.Log("通用分发函数：请求处理完毕，执行后处理...", r.RequestURI, r.Host, r.RemoteAddr)
-}
-
-func megaHandler(w http.ResponseWriter, r *http.Request) {
-	logger.Log("megaHandler=Host=", r.RequestURI, r.Host, r.RemoteAddr)
-}
-
-func abcdHandler(w http.ResponseWriter, r *http.Request) {
-	logger.Log("abcdHandler=Host=", r.RequestURI, r.Host, r.RemoteAddr)
-}
-
 var routesMap = map[string]httpHandleFunc{
 	"/mega": dispatcher(megaHandler),
 	"/abcd": dispatcher(abcdHandler),
@@ -69,4 +44,31 @@ func ListenAndServe(port int) {
 		http.HandleFunc(route, routesMap[route])
 	}
 	http.ListenAndServe(":"+portStr, nil)
+}
+
+// 通用的分发函数（中间件）
+func dispatcher(next httpHandleFunc) httpHandleFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		go indexHandler(w, r, next)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request, next httpHandleFunc) {
+	// 在执行实际的请求处理之前做一些处理
+	logger.Log("通用分发函数：请求到来，执行前处理...", r.RequestURI, r.Host, r.RemoteAddr)
+	// 你可以在这里加入公共的处理逻辑，例如验证、日志记录等
+	// 调用下一个处理函数
+	next(w, r)
+	// 在实际的请求处理之后做一些处理
+	logger.Log("通用分发函数：请求处理完毕，执行后处理...", r.RequestURI, r.Host, r.RemoteAddr)
+}
+
+func megaHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Log("megaHandler=Host=", r.RequestURI, r.Host, r.RemoteAddr)
+}
+
+func abcdHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Log("abcdHandler==", r.RequestURI, r.Host, r.RemoteAddr)
+
+	// logger.Log("abcdHandler=param=", r.)
 }
