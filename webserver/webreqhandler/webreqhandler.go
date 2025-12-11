e9526e9b467b58150e0d2e9e33e733eee7b4b2f1
mega@mac <mega@mac.com>
2025-12-11 11:12:32 +0800

    fix

--- webserver/webreqhandler/webreqhandler.go
+++ webserver/webreqhandler/webreqhandler.go
@@ -8,7 +8,6 @@ import (
 )
 
 var routesMap = map[string]http_common.HttpHandleFunc{
-	// "/mega":        http_common.Dispatcher(megaHandler),
 	"/init_client": http_common.Dispatcher(init_client_handler),
 }
 
@@ -20,27 +19,12 @@ func ListenAndServe(port int) {
 	http_common.ListenAndServe(port, routesMap)
 }
 
-func megaHandler(w http.ResponseWriter, r *http.Request, ip, dataObj map[string]interface{}) {
-	logger.Log("megaHandler=Host=", ip)
-}
-
-type Response struct {
-	Message string `json:"message"`
-}
-
-// 定义示例结构体
-// 根据你的实际需求调整字段类型和标签
-type User struct {
-	Name  string `json:"name"`
-	Age   int    `json:"age"`
-	Email string `json:"email"`
-}
-
 func init_client_handler(w http.ResponseWriter, r *http.Request, ip string, dataObj map[string]interface{}) {
 	logger.Log("init_client_handler=param=Body", ip)
 	var respModel http_common.HttpResponseModel = http_common.HttpResponseModel{Code: error_code.OK}
 	respModel.Data = map[string]interface{}{
 		"need_hotupdate": false, //需要热更新
+		"force_update":   false,
 	}
 	http_common.SendHttpResponseModel(w, respModel)
 }
