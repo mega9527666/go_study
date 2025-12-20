3e85a7e5db7698849a75face27bc5f3c83955366
mega@mac <mega@mac.com>
2025-12-20 16:46:36 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 go build -o ./build/go_webserver webserver/webserver.go
 
 go build -o ./build/go_account_server account_server/account_server.go
+
+go build -o ./build/go_hall_server hall_server/hall_server.go
--- dev.sh
+++ dev.sh
@@ -15,11 +15,21 @@ env=1
 # done;
 
 
+# portList=(
+#   9091
+# )
+# for(( i=0;i<${#portList[@]};i++)) do
+#     port=${portList[i]}
+# echo "port="$port
+# pm2 start ./build/go_account_server --name go_account_server-$port   --output="./logs/go_account_server"-$port".log" --log-date-format="YYYY-MM-DD HH:mm:ss:SSS"   -- $port $env
+# done;
+
+
 portList=(
-  9091
+  9527
 )
 for(( i=0;i<${#portList[@]};i++)) do
     port=${portList[i]}
 echo "port="$port
-pm2 start ./build/go_account_server --name go_account_server-$port   --output="./logs/go_account_server"-$port".log" --log-date-format="YYYY-MM-DD HH:mm:ss:SSS"   -- $port $env
+pm2 start ./build/go_hall_server --name go_hall_server-$port   --output="./logs/go_hall_server"-$port".log" --log-date-format="YYYY-MM-DD HH:mm:ss:SSS"   -- $port $env
 done;
