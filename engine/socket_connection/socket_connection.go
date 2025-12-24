9b5a1fd5557a2540c5809a2925af5f0426db49f0
mega@mac <mega@mac.com>
2025-12-24 15:25:05 +0800

    fix

--- engine/socket_connection/socket_connection.go
+++ engine/socket_connection/socket_connection.go
@@ -68,6 +68,8 @@ func (s *Socket_Connection) ReadMsg() {
 		case websocket.TextMessage:
 			str := string(msg)
 			logger.Log("收到消息 string: ", s.Id, s.Ip, msgType, str)
+			b := []byte(str)
+			s.Send(b)
 		case websocket.BinaryMessage:
 			logger.Log("收到消息 BinaryMessage: ", s.Id, s.Ip, msgType, msg)
 		case websocket.CloseMessage: //websocket.CloseMessage 基本收不到（重要⚠️）大多数情况下： [warn] 读取消息失败: 127.0.0.1 websocket: close 1001 (going away)
